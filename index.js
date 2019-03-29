a86989492d46afef3d70ad6450c3766d6d6f1f7d
Lucas Pereira <luca85perez@gmail.com>
2019-03-29 13:40:09 -0300

    Add cors config

--- index.js
+++ index.js
@@ -10,5 +10,9 @@ const server = new GraphQLServer({
   resolvers,
 });
 server.start({
+  cors: {
+    credentials: true,
+    origin: 'http://localhost:3000',
+  },
   port: process.env.PORT || 4000,
 }, () => console.log(`Server is running`));
